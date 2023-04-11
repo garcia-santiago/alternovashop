7c0947cc22b4cdc7f0f517ea3df1cd5da31d4e38
Santiago Garcia <sgarciavil.29@gmail.com>
2023-04-11 16:15:33 -0500

    fixed img source

--- src/components/ProductsLayout.vue
+++ src/components/ProductsLayout.vue
@@ -4,6 +4,8 @@ import CardImg from './CardImg.vue'
 import productsJSON from '../json/products.json'
 const products = productsJSON.products
 
+const imgs = [ "https://images.ctfassets.net/lcr8qbvxj7mh/2YFBOkIl3scPnFZGw8gLXz/e97cd77969e6ddd699aed3afb1ecb454/CUT-CO_RBED_250_Single-Unit_close_ambien.png", "https://grupodiana.co/wp-content/uploads/2019/12/arroz-valor01.png", "https://copservir.vtexassets.com/arquivos/ids/771951/PAPA-MARGARITA-LIMON_F.png?v=637951129187230000", "https://lecarnegourmet.com/wp-content/uploads/2022/07/costilla-le-carne.jpg"]
+
 </script>
 
 <template>
@@ -13,7 +15,7 @@ const products = productsJSON.products
             :name="product.name" 
             :unit_price="product.unit_price" 
             :stock="product.stock" 
-            :img_url="product.img_url" 
+            :img_url="imgs[index]" 
         />
     </div>
 </div>
