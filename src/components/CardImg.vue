d88dd8fa4bf517b94afd09db1c7ee2a8bbc46447
Santiago Garcia <sgarciavil.29@gmail.com>
2023-04-11 14:09:49 -0500

    mensaje cuando no hay stock

--- src/components/CardImg.vue
+++ src/components/CardImg.vue
@@ -30,7 +30,7 @@ function addToCart(){
     })
 
     if(quantity.value+sum>self_product.stock){
-        console.log("No se puede agregar, no hay stock");
+        mostrarMensaje('red', "No se puede agregar, no hay stock");
     }
     else{
         const newProduct = <ProductCart> {
@@ -42,6 +42,19 @@ function addToCart(){
         storeCart.addToCart(newProduct);
     }
 }
+const tgglMensaje = ref(false);
+const mensaje = ref("");
+const tipo = ref("");
+
+const mostrarMensaje = (type, msg) => {
+    tipo.value = type
+    mensaje.value = msg
+    tgglMensaje.value = true
+
+    setTimeout(()=> { 
+        tgglMensaje.value = false 
+    }, 3000);
+}
 
 </script>
 <template>
@@ -49,12 +62,18 @@ function addToCart(){
     <div class="rounded overflow-hidden shadow-lg tarjeta">
         <img class="w-full h-75" :src="img_url" alt="Imagen Miniatura">
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">{{name}}</div>
+            <div class="font-bold text-xl mb-2">{{name}} - ${{unit_price}}</div>
         </div>
         <div class="px-6 pt-4 pb-2 flex gap-x-1">
             <input min="1" type="number" name="" id="" class="w-1/5 rounded" v-model="quantity">
             <button class="btn w-4/5" @click="addToCart">Add to cart</button>
         </div>
+        <transition name="fade">
+            <div class="text-white py-2 w-75 mx-auto rounded my-2 text-center" v-show="tgglMensaje" 
+                :class="tipo=='green'?'bg-green-500' :tipo=='red' ? 'bg-red-500' : ''">
+                    {{mensaje}}
+            </div>
+        </transition>
     </div>
 
 </template>
